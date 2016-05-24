d3f42fbba1771517e3fb20941eb7b6aa38c0ebd3
pauldiehl <pauldiehl@users.noreply.github.com>
2016-05-24 07:43:25 -0400

    add all

---
+++ public/js/services.js
@@ -0,0 +1,11 @@
+angular.module('movieApp.services',[]).factory('Movie',function($resource){
+    return $resource('http://movieapp-sitepointdemos.rhcloud.com/api/movies/:id',{id:'@_id'},{
+        update: {
+            method: 'PUT'
+        }
+    });
+}).service('popupService',function($window){
+    this.showPopup=function(message){
+        return $window.confirm(message);
+    }
+});
